a93279b63b5c831c25d8622e3b8bb6aa12fb2fcf
mstgnz <mesutgenez@hotmail.com>
2023-12-29 01:16:55 +0300

    load sql

---
+++ testdata/test.sql
@@ -0,0 +1,8 @@
+-- TEST1
+SELECT * FROM assistant_services as ass
+    JOIN assistants as a ON a.id=ass.assistant_id
+    JOIN vehicles as v ON v.id=a.vehicle_id
+    WHERE ass.id=$1;
+
+-- TEST2
+UPDATE assistants SET provider_id = $1 WHERE id = $1;
